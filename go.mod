24d5fbcd4e815bad0500256e8ba21bef6fa5ba13
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2023-05-24 18:32:01 +0900

    first import

---
+++ go.mod
@@ -0,0 +1,27 @@
+module github.com/mattn/feed2twitter
+
+go 1.20
+
+require (
+	github.com/garyburd/go-oauth v0.0.0-20180319155456-bca2e7f09a17
+	github.com/lib/pq v1.10.9
+	github.com/mmcdole/gofeed v1.2.1
+	github.com/uptrace/bun v1.1.13
+	github.com/uptrace/bun/dialect/pgdialect v1.1.13
+)
+
+require (
+	github.com/PuerkitoBio/goquery v1.8.0 // indirect
+	github.com/andybalholm/cascadia v1.3.1 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/mmcdole/goxpp v1.1.0 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/tmthrgd/go-hex v0.0.0-20190904060850-447a3041c3bc // indirect
+	github.com/vmihailenco/msgpack/v5 v5.3.5 // indirect
+	github.com/vmihailenco/tagparser/v2 v2.0.0 // indirect
+	golang.org/x/net v0.4.0 // indirect
+	golang.org/x/sys v0.8.0 // indirect
+	golang.org/x/text v0.5.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,150 @@
+package main
+
+import (
+	"bytes"
+	"context"
+	"database/sql"
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
+	"text/template"
+	"time"
+
+	_ "github.com/lib/pq"
+	"github.com/mmcdole/gofeed"
+	"github.com/uptrace/bun"
+	"github.com/uptrace/bun/dialect/pgdialect"
+
+	"github.com/garyburd/go-oauth/oauth"
+)
+
+const (
+	updateURL = "https://api.twitter.com/1.1/statuses/update.json"
+)
+
+var (
+	oauthClient = oauth.Client{
+		TemporaryCredentialRequestURI: "https://api.twitter.com/oauth/request_token",
+		ResourceOwnerAuthorizationURI: "https://api.twitter.com/oauth/authenticate",
+		TokenRequestURI:               "https://api.twitter.com/oauth/access_token",
+	}
+	dry        = flag.Bool("dry", false, "dry-run")
+	silent     = flag.Bool("s", false, "no post")
+	configFile = flag.String("c", "config.json", "path to config.json")
+	issuesFile = flag.String("f", "issues.json", "path to issues.json")
+)
+
+type Feed2Twitter struct {
+	bun.BaseModel `bun:"table:feed2twitter,alias:f"`
+
+	Feed      string    `bun:"feed,pk,notnull" json:"feed"`
+	GUID      string    `bun:"guid,pk,notnull" json:"guid"`
+	CreatedAt time.Time `bun:"created_at,notnull,default:current_timestamp" json:"created_at"`
+}
+
+func postTweet(token *oauth.Credentials, status string) error {
+	param := make(url.Values)
+	param.Set("status", status)
+	oauthClient.SignParam(token, "POST", updateURL, param)
+	resp, err := http.PostForm(updateURL, url.Values(param))
+	if err != nil {
+		log.Println("failed to post tweet:", err)
+		return err
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != 200 {
+		log.Println("failed to post tweet")
+		return err
+	}
+	return nil
+}
+
+func main() {
+	var skip bool
+	var dsn string
+	var feedURL string
+	var format string
+	var clientToken, clientSecret, accessToken, accessSecret string
+	flag.BoolVar(&skip, "skip", false, "Skip tweet")
+	flag.StringVar(&dsn, "dsn", "", "Database source")
+	flag.StringVar(&feedURL, "feed", "", "Feed URL")
+	flag.StringVar(&format, "format", "{{.Title}}\n{{.Link}}", "Tweet Format")
+	flag.StringVar(&clientToken, "client-token", os.Getenv("FEED2TWITTER_CLIENT_TOKEN"), "Twitter ClientToken")
+	flag.StringVar(&clientSecret, "client-secret", os.Getenv("FEED2TWITTER_CLIENT_SECRET"), "Twitter ClientSecret")
+	flag.StringVar(&accessToken, "access-token", os.Getenv("FEED2TWITTER_ACCESS_TOKEN"), "Twitter AccessToken")
+	flag.StringVar(&accessSecret, "access-secret", os.Getenv("FEED2TWITTER_ACCESS_SECRET"), "Twitter AccessSecret")
+	flag.Parse()
+
+	t := template.Must(template.New("").Parse(format))
+
+	token := &oauth.Credentials{
+		Token:  accessToken,
+		Secret: accessSecret,
+	}
+
+	db, err := sql.Open("postgres", dsn)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	bundb := bun.NewDB(db, pgdialect.New())
+	defer bundb.Close()
+
+	_, err = bundb.NewCreateTable().Model((*Feed2Twitter)(nil)).IfNotExists().Exec(context.Background())
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	feed, err := gofeed.NewParser().ParseURL(feedURL)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		return
+	}
+
+	for _, item := range feed.Items {
+		if item == nil {
+			break
+		}
+
+		fi := Feed2Twitter{
+			Feed: feedURL,
+			GUID: item.GUID,
+		}
+		_, err := bundb.NewInsert().Model(&fi).Exec(context.Background())
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+
+		var buf bytes.Buffer
+		err = t.Execute(&buf, &item)
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+		content := buf.String()
+		runes := []rune(content)
+		if len(runes) > 140 {
+			item.Title = string(item.Title[:len(item.Title)-len(runes)+140])
+			buf.Reset()
+			err = t.Execute(&buf, &item)
+			if err != nil {
+				log.Println(err)
+				continue
+			}
+		}
+		if skip {
+			log.Printf("%q", buf.String())
+			continue
+		}
+		err = postTweet(token, buf.String())
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+	}
+}
